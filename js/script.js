7f1cead28e247d57949ef0e7f326c591259bbbb2
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-03-16 15:03:05 +0100

    add js for sidebar

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 const navigation = document.querySelector("nav");
-const sidebar = document.querySelector("sidebar");
+const sidebar = document.querySelector(".sidebar");
 
 let screenVH = vhToPixels(93);
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > screenVH) {
     navigation.classList.add("transform-scroll-nav");
+    sidebar.classList.add("transform-sidebar");
   } else {
     navigation.classList.remove("transform-scroll-nav");
+    sidebar.classList.remove("transform-sidebar");
   }
 });
 
