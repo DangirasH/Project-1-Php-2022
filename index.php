52a98a0cb7ea77e94a4f4ab0b4fcf2436550ad28
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-03-22 21:00:42 +0100

    add close button on burger

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -13,7 +12,6 @@
   <link rel="stylesheet" href="styles/nav.css" />
   <link rel="stylesheet" href="styles/contact.css" />
 </head>
-
 <body>
   <header>
     <aside class="responsive-top">
